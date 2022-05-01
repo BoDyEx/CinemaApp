a73b937660bdbbbbcd260fefc01333024a9cf885
BoDyEx <201927036@std.sci.cu.edu.eg>
2022-05-01 18:35:19 +0200

    login_signin

--- component/homepage.js
+++ component/homepage.js
@@ -5,6 +5,16 @@ export default function homepage({ navigation }) {
     return (
         <View style={styles.container}>
         <ImageBackground source={cinema} resizeMode="cover" style={styles.imeg}>
+        <View style={styles.Button}>
+          <Button title="Log in" color="#0000FF" onPress={() => {
+            navigation.navigate("Log In");
+          }}></Button>
+        </View>
+        <View style={styles.Button1}>
+          <Button title="Sign up" color="#0000FF" onPress={() => {
+            navigation.navigate("Register");
+          }}></Button>
+        </View>
         </ImageBackground>
       </View>
     );
@@ -18,5 +28,20 @@ export default function homepage({ navigation }) {
       padding: 20,
       alignItems: "center",
       
-    }
+    },
+    Button1:{
+        width: 100,
+        marginLeft:250,
+        marginTop:-580,
+  
+
+    },
+    Button:{
+        width: 100,
+        paddingVertical:545,
+        marginRight:180,
+     
+
+    },
+    
   });
--- firebase_config.js
+++ firebase_config.js
@@ -1,16 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
-//"^9.6.11"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAXHu1ve04vj29_2knpEM3NlDnCBjhmK5M",
     authDomain: "project303-9e84c.firebaseapp.com",
@@ -21,9 +13,7 @@ const firebaseConfig = {
     measurementId: "G-TMZ5V3XK7D"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
