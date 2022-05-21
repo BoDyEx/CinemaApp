f3e3500bf147db83db7d0cbe3964463996092a56
BoDyEx <201927036@std.sci.cu.edu.eg>
2022-05-21 17:57:39 +0200

    search comdy

--- component/screens/ComdyMovies.js
+++ component/screens/ComdyMovies.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import {   View, StyleSheet,FlatList,TouchableOpacity} from 'react-native';
+import {   View, StyleSheet,FlatList,TouchableOpacity,TextInput,Text} from 'react-native';
 import { collection, getDocs} from 'firebase/firestore/lite';
 import FilmItem from '../FilmItem';
 import { db } from '../../db/Config';
@@ -19,12 +19,42 @@ const ComdyMovie=({ navigation, route })=>{
   }, []);
 
   
+  const[searchItem,setsearchItem]=useState();
+
+  const search = (searchItem) => {    
+    if(searchItem.length == 0){
+      setmovies(movies); 
+    }else {
+      let s = "";
+      s = searchItem;
+      let h = 0;
+      let data = [];
+
+      for (let i = 0; i < movies.length; i++) {
+        if( movies[i].name_film.match(s)){
+          data[h] = movies[i]; 
+          h++
+        }
+          
+      }
+      setmovies(data);
+    }
       
+  };
 
   return(
     
     <View style={styles.mainContanier}>
+         <TextInput
+              placeholder="Search"
+              onChangeText={setsearchItem} 
+              style={styles.search}
+         
+          />
+          <TouchableOpacity style={styles.button}  onPress={()=>search(searchItem)} >
+          <Text style={styles.txtbtn}>search</Text>
 
+          </TouchableOpacity>
         <FlatList 
           data={movies}
           renderItem={({item})=>(
@@ -57,7 +87,32 @@ const styles=StyleSheet.create({
     txt:{
         fontSize:0,
         textAlign:"center"
+    },search:{
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      borderRadius:15,
+      borderColor:'#53E8C6',
+      textAlign: 'left',
+      color:"white"
     },
+    button:{
+  width:"20%",
+  padding:10,
+  borderRadius:30,
+  marginTop:0,
+  marginHorizontal:"40%",
+  borderColor:"#53E8C6",
+  borderWidth:0.5,
+   
+      
+    },txtbtn:{
+      color:"#53E8C6",
+      fontSize:16,
+      alignSelf: "center",
+      fontWeight:"bold", 
+    }
 
 
 
